331585fc98102e462a53a48eb6838d8d7745cf36
LeonMuellerGT <leon.dennis.mueller@gmail.com>
2017-11-30 14:34:14 +0100

    first version

---
+++ app/src/main/java/de/fh_bielefeld/timetable/Table.java
@@ -0,0 +1,35 @@
+package de.fh_bielefeld.timetable;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Table extends Fragment {
+
+    private ListView l;
+
+    public Table() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_table, container, false);
+
+        l = (ListView) view.findViewById(R.id.listview);
+
+        //l.addView();
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/de/fh_bielefeld/timetable/callendar.java
@@ -0,0 +1,18 @@
+package de.fh_bielefeld.timetable;
+
+/**
+ * Created by Leon on 30.11.2017.
+ */
+
+public class callendar {
+    private String name = "";
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String n){
+        this.name = n;
+    }
+
+}
